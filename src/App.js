f79b0116c08c668c04e534350fad294f5373b50b
Maxim Svistunov <svistunovms@lad24.ru>
2020-01-23 13:20:48 +0300

    Initial app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <div className="main-content">
+        Hello
+      </div>
+    </div>
+  );
+};
+
+export default App;
